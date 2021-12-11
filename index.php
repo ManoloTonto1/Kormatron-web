1c2cb343e3cd27e8fd84614fd0a169c2b2f2b7f1
DikkeBoktor <94758288+DikkeBoktor@users.noreply.github.com>
2021-12-11 22:58:55 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,26 @@
+<!-- import the page header -->
+<?php include('src/header.html') ?>
 
-<?php include("src/mainmenu.php"); ?>
+<form action="mainmenu.php" method="post">
+  <div class="imgcontainer">
+    <img src="" alt="" class="">
+  </div>
 
+  <div class="container">
+    <label for="uname"><b>Username</b></label>
+    <input type="text" placeholder="Enter Username" name="bokkie" required>
+
+    <label for="psw"><b>Password</b></label>
+    <input type="password" placeholder="Enter Password" name="WW" required>
+
+    <button type="submit">Login</button>
+    <label>
+      <input type="checkbox" checked="checked" name="remember"> Remember me
+    </label>
+  </div>
+
+  <div class="container" style="background-color:#f1f1f1">
+    <button type="button" class="cancelbtn">Cancel</button>
+    <span class="psw">Forgot <a href="#">password?</a></span>
+  </div>
+</form>
