8af1aa7c8a559555c99d508d4f06d6547b51e9e1
DikkeBoktor <94758288+DikkeBoktor@users.noreply.github.com>
2021-12-15 13:07:37 +0100

    Login

--- index.php
+++ index.php
@@ -7,20 +7,27 @@
   </div>
 
   <div class="uname-password">
-    <label for="uname"><b>Username</b></label>
-    <input type="text" placeholder="Enter Username" name="bokkie" required>
-
-    <label for="psw"><b>Password</b></label>
-    <input type="password" placeholder="Enter Password" name="WW" required>
-
-    <button type="submit">Login</button>
+  <img class="login-image" src="src/img/logoKormaTron.png" alt="logo">
+  <div class="login-slider">
+  <label class="switch">
+  <input type="checkbox">
+  <span class="slider round"></span>
+</label>
+</div>
+  <br>
+    <label class="login-label" for="uname"><b>Username</b></label>
+    <br>
+    <input class="login-input" type="text" placeholder="Enter Username" name="bokkie" required>
+    <br>
+    <label class="login-label" for="psw"><b>Password</b></label>
+    <br>
+    <input class="login-input" type="password" placeholder="Enter Password" name="WW" required>
+    <br>
+    <br>
+    <br>
+    <button class="login-button" type="submit">Login</button>
     <label>
-      <input type="checkbox" checked="checked" name="remember"> Remember me
+      <input class="remember-me" type="checkbox" checked="checked" name="remember"> Remember me
     </label>
-  </div>
-
-  <div class="container" style="background-color:#f1f1f1"> 
-    <button type="button" class="cancelbtn">Cancel</button>
-    <span class="psw">Forgot <a href="#">password?</a></span>
   </div>
 </form>
